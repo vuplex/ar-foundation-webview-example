6dfc819de297035c29d674da5d81d7d196a6d915
Tim Mowrer <timm@unity3d.com>
2018-08-13 09:40:02 -0700

    [XR] Add comments and tooltips

--- Assets/SaveCameraImage.cs
+++ Assets/SaveCameraImage.cs
@@ -1,26 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.XR.ARFoundation;
 using System.IO;
 
+/// <summary>
+/// Saves an AR Camera Background image to a file.
+/// Demonstrates blitting the texture to a <c>RenderTexture</c>
+/// and then copying from the GPU to a CPU <c>Texture2D</c>.
+/// Finally, the image can then be saved to an image on disk.
+/// </summary>
 public class SaveCameraImage : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("The filename to save the camera background to.")]
     string m_Filename;
 
+    /// <summary>
+    /// The filename to save the camera background to.
+    /// </summary>
+    public string filename
+    {
+        get { return m_Filename; }
+        set { m_Filename = value; }
+    }
+
     [SerializeField]
+    [Tooltip("The camera background which controls the camera image.")]
     ARCameraBackground m_ARCameraBackground;
 
+    /// <summary>
+    /// Get the <c>ARCameraBackground</c> which controls the camera image.
+    /// </summary>
+    public ARCameraBackground arCameraBackground
+    {
+        get { return m_ARCameraBackground; }
+        private set { m_ARCameraBackground = value; }
+    }
+
     [SerializeField]
+    [Tooltip("The RenderTexture to blit the camera image to.")]
     RenderTexture m_RenderTexture;
 
+    /// <summary>
+    /// Get or set the <c>RenderTexture</c> to blit to.
+    /// </summary>
+    public RenderTexture renderTexture
+    {
+        get { return m_RenderTexture; }
+        set { m_RenderTexture = value; }
+    }
+
     [SerializeField]
+    [Tooltip("The image format to use when saving to disk.")]
     ImageFormat m_ImageFormat;
 
-    Texture2D m_CachedCameraTexture;
-
+    /// <summary>
+    /// Defines an image format to use when saving to disk.
+    /// </summary>
     public enum ImageFormat
     {
         EXR,
@@ -28,18 +66,35 @@ public class SaveCameraImage : MonoBehaviour
         PNG
     }
 
+    /// <summary>
+    /// Get or set the image format used when saving to disk.
+    /// </summary>
+    public ImageFormat imageFormat
+    {
+        get { return m_ImageFormat; }
+        set { m_ImageFormat = value; }
+    }
+
+    Texture2D m_CachedCameraTexture;
+
+    /// <summary>
+    /// Copies the <c>ARCameraBackground</c> to a texture and
+    /// saves it to <see cref="filename"/>.
+    /// </summary>
     public void SaveCameraTexture()
     {
-        if (m_RenderTexture == null || m_ARCameraBackground == null || string.IsNullOrEmpty(m_Filename))
+        if (m_RenderTexture == null || m_ARCameraBackground == null)
             return;
 
         EnsureTextureCreated();
 
-        BlitToRenderTexture(m_RenderTexture, m_ARCameraBackground);
-
+        // Copy the RenderTexture from GPU to CPU
         if (SystemInfo.supportsAsyncGPUReadback)
         {
             Debug.Log("Using async GPU readback.");
+
+            // On platforms that support it, we can copy from GPU to CPU
+            // using an asynchronous request, which doesn't stall the main thread.
             var request = AsyncGPUReadback.Request(m_RenderTexture);
             StartCoroutine(HandleGPUReadback(request));
         }
@@ -47,7 +102,7 @@ public class SaveCameraImage : MonoBehaviour
         {
             Debug.Log("Async GPU readback not supported. Using ReadPixels (slow).");
 
-            // Copy the RenderTexture from GPU to CPU
+            // On other platforms, we have to copy from GPU to CPU on the main thread.
             var activeRenderTexture = RenderTexture.active;
             RenderTexture.active = m_RenderTexture;
             m_CachedCameraTexture.ReadPixels(new Rect(0, 0, m_RenderTexture.width, m_RenderTexture.height), 0, 0);
@@ -57,15 +112,29 @@ public class SaveCameraImage : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Blits the <c>ARCameraBackground</c> to a <c>RenderTexture</c>. Fast.
+    /// </summary>
+    /// <param name="renderTexture">The target <c>RenderTexture</c>.</param>
+    /// <param name="cameraBackground">The <c>ARCameraBackground</c> which controls the camera image.</param>
     public static void BlitToRenderTexture(RenderTexture renderTexture, ARCameraBackground cameraBackground)
     {
-        if (renderTexture == null || cameraBackground == null || cameraBackground.material == null)
-            return;
+        if (renderTexture == null)
+            throw new ArgumentNullException("renderTexture");
+
+        if (cameraBackground == null)
+            throw new ArgumentNullException("cameraBackground");
 
         // Copy the camera background to a RenderTexture
         Graphics.Blit(null, renderTexture, cameraBackground.material);
     }
 
+    /// <summary>
+    /// Writes a <c>Texture2D</c> to a file.
+    /// </summary>
+    /// <param name="texture">The <c>Textur2D</c> to save.</param>
+    /// <param name="path">The path to write the image to.</param>
+    /// <param name="imageFormat">The image format to use.</param>
     public static void WriteTextureToFile(Texture2D texture, string path, ImageFormat imageFormat)
     {
         if (texture == null)
@@ -96,6 +165,22 @@ public class SaveCameraImage : MonoBehaviour
         File.WriteAllBytes(path, bytes);
     }
 
+    void OnCameraFrameReceived(ARCameraFrameEventArgs eventArgs)
+    {
+        if (renderTexture != null && arCameraBackground != null)
+            BlitToRenderTexture(renderTexture, arCameraBackground);
+    }
+
+    void OnEnable()
+    {
+        ARSubsystemManager.cameraFrameReceived += OnCameraFrameReceived;
+    }
+
+    void OnDisable()
+    {
+        ARSubsystemManager.cameraFrameReceived -= OnCameraFrameReceived;
+    }
+
     IEnumerator HandleGPUReadback(AsyncGPUReadbackRequest request)
     {
         if (!request.done)
@@ -111,8 +196,11 @@ public class SaveCameraImage : MonoBehaviour
 
     void WriteTextureToFile()
     {
-        var path = Path.Combine(Application.persistentDataPath, m_Filename);
-        WriteTextureToFile(m_CachedCameraTexture, path, m_ImageFormat);
+        if (string.IsNullOrEmpty(filename))
+            return;
+
+        var path = Path.Combine(Application.persistentDataPath, filename);
+        WriteTextureToFile(m_CachedCameraTexture, path, imageFormat);
         Debug.LogFormat("Saved texture to \"{0}\"", path);
     }
 
