26cf482116b67e8ae4f6014fb921d12a6759e659
Nate Hill <binarynate@gmail.com>
2022-10-11 23:15:44 -0500

    Updated WebViewARAnchor.cs for 3D WebView v4.3.

--- Assets/Vuplex/ARWebViewExample/Scripts/WebViewARAnchor.cs
+++ Assets/Vuplex/ARWebViewExample/Scripts/WebViewARAnchor.cs
@@ -10,31 +10,31 @@ class WebViewARAnchor : MonoBehaviour {
         // This is done so that it doesn't create other webviews in response to interacting with the webview.
         _setAnchorCreatorEnabled(false);
 
-        // Create a 0.6 x 0.3 WebViewPrefab.
+        // Instantiate a 0.6 x 0.3 WebViewPrefab.
+        // https://developer.vuplex.com/webview/WebViewPrefab#Instantiate
         var webViewPrefab = WebViewPrefab.Instantiate(0.6f, 0.3f);
         webViewPrefab.transform.SetParent(transform, false);
         webViewPrefab.transform.localPosition = new Vector3(0, 0.7f, 0);
         webViewPrefab.transform.localEulerAngles = new Vector3(0, 180, 0);
 
-        // Add the keyboard under the WebViewPrefab.
+        // Add an on-screen keyboard under the webview.
+        // https://developer.vuplex.com/webview/Keyboard
         var keyboard = Keyboard.Instantiate();
         keyboard.transform.parent = webViewPrefab.transform;
         keyboard.transform.localPosition = new Vector3(0, -0.31f, 0);
-        keyboard.transform.localEulerAngles = new Vector3(0, 0, 0);
-        // Hook up the keyboard so that characters are routed to the main webview.
-        keyboard.InputReceived += (sender, e) => webViewPrefab.WebView.HandleKeyboardInput(e.Value);
+        keyboard.transform.localEulerAngles = Vector3.zero;
 
-        // Wait for the WebViewPrefab to initialize, because the WebViewPrefab.WebView property
-        // is null until the prefab has initialized.
+        // Wait for the prefab to initialize because its WebView property is null until then.
+        // https://developer.vuplex.com/webview/WebViewPrefab#WaitUntilInitialized
         await webViewPrefab.WaitUntilInitialized();
+
+        // After the prefab has initialized, you can use the IWebView APIs via its WebView property.
+        // https://developer.vuplex.com/webview/IWebView
         webViewPrefab.WebView.LoadUrl("https://www.google.com");
     }
 
-    void OnDestroy() {
-
-        // Re-enable the anchor creator so that a new webview can be created.
-       _setAnchorCreatorEnabled(true);
-    }
+    // Re-enable the anchor creator so that a new webview can be created.
+    void OnDestroy() => _setAnchorCreatorEnabled(true);
 
     void _setAnchorCreatorEnabled(bool enabled) {
 
